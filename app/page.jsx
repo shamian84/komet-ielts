315ccfd095056c977bb0824d74fac22eccc187fb
shami <shamialam8423@gmail.com>
2026-02-02 18:27:48 +0530

    error resolved

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,6 @@ import ExamStructure from "@/components/sections/ExamSturcture";
 import TestDatesPage from "@/components/sections/TestDates";
 import PreparingForIELTS from "@/components/sections/PreparationDetails";
 import TrainingPage from "@/components/sections/Training";
-import ResourcesPage from "@/not-needed/Resources";
 import Footer from "@/components/layout/Footer";
 
 export default function Home() {
